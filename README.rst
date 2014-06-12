247d8b72be562285175a686b27600f1a8a7753a4
Douglas MacDougall <douglas.macdougall@moesol.com>
2014-06-12 14:05:45 -0700

    initial

---
+++ README.rst
@@ -0,0 +1,27 @@
+===========
+SQLStrainer
+===========
+
+SQLStrainer filters SQLAlchemy Query objects based any column or hybrid property of a related model.
+
+    from sqlstrainer import strainer
+    strainer = sqlstrainer(mydb)
+    query.filter(strainer.strain(request.args))
+
+Query
+=====
+
+Need to know the query
+
+Relations
+=========
+
+Automatically build a relationship map
+
+
+Contributors:
+=============
+
+Douglas MacDougall <douglas.macdougall@moesol.com>
+
+see @Authors
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='SQLStrainer',
+    version='0.0.1',
+    author='Douglas MacDougall',
+    author_email='douglas.macdougall@moesol.com',
+    packages=['sqlstrainer'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/sqlstrainer/',
+    license='LICENSE',
+    description='Easily filter SQLAlchemy queries based on HTTP request args.',
+    long_description=open('README').read(),
+    install_requires=[
+        "SQLAlchemy >= 0.8.0"
+    ],
+)
